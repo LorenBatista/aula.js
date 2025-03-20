f7b39aafa75b4605e94c2251affec86a05e2b622
Loren <129312035+LorenBatista@users.noreply.github.com>
2025-03-20 20:28:11 +0000

    https://github.com/LorenBatista/aula.js.git

---
+++ 04 - Funcoes/aulafuncoes.js
@@ -0,0 +1,12 @@
+function sayMyName(name){
+    console.log('Your name is:' + name);
+}
+
+sayMyName('Loren');
+sayMyName('Victor');
+
+function quadrado(valor){
+    return valor * valor;
+}
+
+const quadradoDeDez = quadrado(10);
